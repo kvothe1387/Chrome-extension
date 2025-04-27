76a4bdfc85e13be8df577972342d202dac264888
David McCullough <dmccullough488@gmail.com>
2025-04-26 20:53:24 -0500

    Logged out the items in the myLeads array using a for loop

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function () {
-  const inputValue = inputEl.value
-  myLeads.push(inputValue)
+  myLeads.push(inputEl.vaule)
   console.log(myLeads)
+
+  for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+  }
 })
 
