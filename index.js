7b11d84e33054a265233c6113fb217fe4483744e
David McCullough <dmccullough488@gmail.com>
2025-04-26 21:56:11 -0500

    add <a> tag and make link open in a new tab

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = ""
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>"
+    listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    console.log(listItems)
   }
   ulEl.innerHTML = listItems
-} 
+}
