b6f9a2ec400ae8952d4412b5deeff00280ad312d
David McCullough <dmccullough488@gmail.com>
2025-04-28 15:36:50 -0500

    created tab btn and save tab feature

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
   renderLeads(myLeads)
 }
+
+const tabs = [
+  { url: "https://www.linkedin.com/in/davidmc1387/" }
+]
+
+tabBtn.addEventListener("click", function () {
+  myLeads.push(tabs[0].url
+
+  )
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  renderLeads(myLeads)
+})
 
 function renderLeads(leads) {
   let listItems = ""
@@ -34,6 +47,6 @@ inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
   inputEl.value = ""
   localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  renderLeads(myLeads)
+  renderLeads(myLeadsf)
 })
 
